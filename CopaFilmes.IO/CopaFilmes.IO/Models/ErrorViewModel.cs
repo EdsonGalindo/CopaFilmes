c48db011912bc1e527b8442fb708f5acc85562b2
Edson Galindo <edcg@hotmail.com.br>
2018-08-29 23:42:50 -0300

    Inclusão do projeto do Site Asp Net Core MVC.

---
+++ CopaFilmes.IO/CopaFilmes.IO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CopaFilmes.IO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
